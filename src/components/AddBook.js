f4c5562f1c59fdea98696faee9ecb3a1ced7b8a5
Chanidapa Wongtep <96721815+chawongr@users.noreply.github.com>
2022-10-31 03:21:21 +0700

    Update AddBook.js

--- src/components/AddBook.js
+++ src/components/AddBook.js
@@ -32,7 +32,7 @@ const AddBook = () => {
 
   const sendRequest = async () => {
     await axios
-      .post("http://localhost:5000/books", {
+      .post("http://localhost:80/api/books", {
         name: String(inputs.name),
         author: String(inputs.author),
         description: String(inputs.description),
